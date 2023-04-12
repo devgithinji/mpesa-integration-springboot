63193298b076872160bd3973460b60eca6486e2b
dennis githinji <wakahiad@gmail.com>
2023-04-12 18:59:03 +0300

    initial commit

---
+++ src/main/java/com/densoft/darajaapi/DarajaApiApplication.java
@@ -0,0 +1,13 @@
+package com.densoft.darajaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarajaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DarajaApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/densoft/darajaapi/DarajaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.densoft.darajaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DarajaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
