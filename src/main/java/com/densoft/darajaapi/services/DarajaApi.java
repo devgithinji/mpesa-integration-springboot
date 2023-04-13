b7ca7c4f717c5129948a7f0cba66ebb0f02d1239
dennis githinji <wakahiad@gmail.com>
2023-04-13 11:07:04 +0300

    register url

--- src/main/java/com/densoft/darajaapi/services/DarajaApi.java
+++ src/main/java/com/densoft/darajaapi/services/DarajaApi.java
@@ -1,7 +1,11 @@
 package com.densoft.darajaapi.services;
 
 import com.densoft.darajaapi.dtos.AccessTokenResponse;
+import com.densoft.darajaapi.dtos.RegisterUrlRequest;
+import com.densoft.darajaapi.dtos.RegisterUrlResponse;
 
 public interface DarajaApi {
     AccessTokenResponse getAccessToken();
+
+    RegisterUrlResponse registerUrl();
 }
